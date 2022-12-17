6a3556cdf56f442e927c652e36790b980cc7cbf4
Elroi Villarias <villariaselroi@gmail.com>
2022-12-17 16:52:21 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -10,6 +12,37 @@ public class SimpleCalcGUI extends JFrame{
     private JTextField tfNumber2;
     private JButton btnCompute;
 
+    public SimpleCalcGUI() {
+        this.setTitle("Simple Calculator");
+
+        cbOperations.addItem("+");
+        cbOperations.addItem("-");
+        cbOperations.addItem("*");
+        cbOperations.addItem("/");
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(tfNumber1.getText());
+                int num2 = Integer.parseInt(tfNumber2.getText());
+                String sym = cbOperations.getSelectedItem().toString();
+                int result;
+
+                if(sym == "+"){
+                    result = num1 + num2;
+                } else if (sym == "-") {
+                    result = num1 - num2;
+                } else if (sym == "*") {
+                    result = num1 * num2;
+                } else {
+                    result = num1 / num2;
+                }
+
+                lblResult.setText(String.valueOf(result));
+            }
+
+        });
+    }
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setContentPane(app.panel1);
